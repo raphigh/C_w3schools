6fad071b27931cc1941e8d0d1eeb16c93685ef48
raphy <ra.phi.mousa.v.nia@gmail.com>
2023-08-08 00:30:15 +0330

    git variables commited

---
+++ 004C_variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	//declare int
+	int myInt;
+	//assign int
+	myInt = 5;
+	//declare and assign
+	float myFloat = 3.14151926;
+	char myChar = 'D';
+	printf("my int is %d,my float is %f and my char is %c",myInt,myFloat,myChar);
+	return 0;
+}
