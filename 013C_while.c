be5a8270a39add6af0fa06072ca7a3c79ea09f91
raphy <ra.phi.mousa.v.nia@gmail.com>
2023-08-08 21:07:51 +0330

    while and do while commited

---
+++ 013C_while.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+	
+	int i = 5;
+	while(i > 0){
+		printf("%i\n",i);
+		i--;
+	}
+	
+	int j = 1;
+	do{
+		printf("%i\n",j*j);
+		j++;
+	}while(j < 11);
+
+	
+	
+	return 0;
+}
