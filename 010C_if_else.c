5feba83da690fe45de14c9777e84ae4e01b5fedd
raphy <ra.phi.mousa.v.nia@gmail.com>
2023-08-08 19:24:56 +0330

    condition commited

---
+++ 010C_if_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int x = 10;
+	int y = 5;
+	if (x==y){
+		printf("x and y are equal");
+	}else if(x<y){
+		printf("x is lower than y");
+	}else{
+		printf("x is greater than y");
+	}
+	return 0;
+}
