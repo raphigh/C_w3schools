0a843a3ed5d28e89e79a48e03a212666eed10387
raphy <ra.phi.mousa.v.nia@gmail.com>
2023-08-08 15:12:24 +0330

    data convertion added

---
+++ 006C_dataConvertion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+	/*
+	int x = 5;
+	int y = 2;
+	int div = 5/2;
+
+	printf("%d",div);//outputs 2
+
+
+	int myInt = 9.99;
+	printf("\n %d",myInt);//outputs 9
+
+	float div = 5 / 2;
+	printf("\n %f",div); //outputs 2.0000
+
+	float div = (float) 5 / 2;
+	printf("\n %f",div);//outputs 2.5000
+	*/
+
+	int num1 = 5;
+	int num2 = 2;
+	float div = (float) num1/num2;
+	printf("%.1f",div);//outputs 2.5
+
+	return 0;
+}
