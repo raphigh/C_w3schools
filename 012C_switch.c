f23fcdb8dc0f9428544b807e0a447bae4bc7e363
raphy <ra.phi.mousa.v.nia@gmail.com>
2023-08-08 21:00:48 +0330

    switch commited

---
+++ 012C_switch.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+	int day = 4;
+	switch(day){
+		case 1:
+			printf("sat");
+			break;
+		case 2:
+			printf("sun");
+			break;
+		default:
+			printf("none");
+			break;
+			
+	}
+	return 0;
+}
